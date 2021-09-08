2cfdb41039e332c66ea14c2c0ae2f7a80c351f72
Amanda <10932525@uvu.edu>
2021-09-08 17:12:18 -0600

    little longer

--- New Unity Project/Assets/Scripts/PlayerControl.cs
+++ New Unity Project/Assets/Scripts/PlayerControl.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
   public float speed = 10.5f;
-public float turnspeed;
+  public float turnspeed;
+  public float hInput;
+  public float vInput;
   
     // Start is called before the first frame update
     void Start()
     {
-     turnspeed =11.2f;
-
-        print(speed);
-
-        print(turnspeed);   
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-         transform.Translate(Vector3.forward * speed);  
+      hInput = Input.GetAxis("Horizontal");
+      vInput = Input.GetAxis("Vertical");
+      //Move the tank left and right
+       transform.Rotate(Vector3.up, turnspeed * hInput * Time.deltaTime);
+       //Move the tank forward and back
+       transform.Translate(Vector3.forward * speed * Time.deltaTime* vInput);  
     }
 }
