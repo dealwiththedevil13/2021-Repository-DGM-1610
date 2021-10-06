1896b31c2bc72ac48236ee892a5e4c5015f65d95
Amanda <10932525@uvu.edu>
2021-10-06 17:12:58 -0600

    10/6/21 Camera Movement and jumping

--- Prototype 3-First Person Game/Assets/Scripts/PlayerController.cs
+++ Prototype 3-First Person Game/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed, jumpForce;         //movement speed in units per second
+    public float moveSpeed;        //movement speed in units per second
     public float jumpForce;         //force applied upwards
 
     public float lookSensitivity;           //Mouse look sensitivity
-    public float maxlookX;      //loewest down we can look
+    public float maxLookX;      //loewest down we can look
     public float minLookX;      //highest up we can look
     public float rotX;          //curent x rotation
 
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         //Get components
-        Camera= camera.main;
+        camera = Camera.main;
         rb=GetComponent<Rigidbody>();
     }
 
@@ -28,19 +28,44 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+        CamLook();
+
+        if(Input.GetButtonDown("Jump"))
+            Jump();
     }
 
+void FixedUpdate()
+{
+    if(Input.GetButtonDown)
+}
     void Move()
     {
         float x= Input.GetAxis("Horizontal") * moveSpeed;
          float z= Input.GetAxis("Vertical") * moveSpeed;
 
-        rb.velocity = new Vector3(x,rb.velocity.y, z);
+        // rb.velocity = new Vector3(x,rb.velocity.y, z);- Old code
+
+        Vector3 dir = transform.right * x + transform.forward * z;
+        rb.velocity = dir;
+
+
     }
 
     void CamLook()
     {
-        float y= Input.GetAxis("Mouse X") * lookSansitivity;
+        float y= Input.GetAxis("Mouse X") * lookSensitivity;
         rotX -= Input.GetAxis("Mouse Y") * lookSensitivity;
+        
+        rotX = Mathf.Clamp(rotX, minLookX,maxLookX);
+        camera.transform.localRotation = Quaternion.Euler(-rotX,0,0);
+        transform.eulerAngles += Vector3.up *y;
+    }
+
+    void Jump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if(Physics.Raycast(ray,1.1f))
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);  //deleted , ForceMode.Impulse
     }
 }
