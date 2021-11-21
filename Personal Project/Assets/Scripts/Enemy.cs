36f6e896b00740803e8c78329944e87783c8fd00
Amanda <10932525@uvu.edu>
2021-11-21 13:49:06 -0700

    Personal Project enemy script pt 1

--- Personal Project/Assets/Scripts/Enemy.cs
+++ Personal Project/Assets/Scripts/Enemy.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
 
 public class Enemy : MonoBehaviour
 {
+    public int curHp, maxHp;
+    public int moveSpeed, attackRange, ypathOffset;
+
+    private List<Vector3> path;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void updatePath()
+    {
+        NavMeshPath navMeshPath = new NavMeshPath();
+        NavMesh.CalculatePath(transform.position, target.transform.position, NavMesh.AllAreas, navMeshPath);
+
+        path = NavMeshPath.corners.ToList();
+    }
     // Update is called once per frame
     void Update()
     {
