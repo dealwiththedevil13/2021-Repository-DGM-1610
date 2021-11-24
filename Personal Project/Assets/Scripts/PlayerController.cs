874ade0666ea22a55f6e223179f7a7f5c08b8c85
Amanda <10932525@uvu.edu>
2021-11-23 20:32:38 -0700

    Personal project Player control pt 2

--- Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public float turnspeed;
+
     // Start is called before the first frame update
     void Start()
     {
+        turnspeed = ;
+
+            print(speed);
+            print(turnspeed);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.translate(Vector3.forward* speed * Time.deltaTime);
     }
 }
