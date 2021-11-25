cf33c4909cae1e91994beacfd9cb6a97d02db213
Amanda <10932525@uvu.edu>
2021-11-24 20:43:00 -0700

    Personal Project Pickup

--- Personal Project/Assets/Scripts/Pickup.cs
+++ Personal Project/Assets/Scripts/Pickup.cs
@@ -4,12 +4,47 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    public PickupType type;
+    public int value;
+
+    [Header("Bobbing")]
+    public float rotationSpeed;
+    public float bobSpeed;
+    public float bobHeight;
+    private bool bobbingUp;
+
+    private Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
+    public enum PickupType
+    {
+        cotton,
+
+        page,
+
+        Health
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+            switch (type)
+            {
+                case PickupType.Health:
+                player.GiveHealth(value);
+                break;
+                
+
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
